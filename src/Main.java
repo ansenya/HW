422157ddc7c6bc3f624d503caa474352446ad845
ansenya <ars.anoshin@gmail.com>
2022-09-30 10:31:03 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a;
+        Scanner in = new Scanner(System.in);
+        int a=1, b=1, c=0;
+        int n = in.nextInt ();
+        for (int i= 1; i<n-1; ++i){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        System.out.println(c);
     }
 }
