e39279f6fc0c0d94ce761d88759c33fd78d82ed7
ansenya <ars.anoshin@gmail.com>
2022-11-07 22:01:22 +0300

    //

--- src/teacher.java
+++
@@ -1,8 +0,0 @@
-public class teacher {
-    public int age = 0;
-    public String lesson = " ", name = " ";
-
-    void meow(){
-        System.out.println("Меня зовут " + name + ". Мне " + age+ " лет." + " Я преподаю " + lesson);
-    }
-}
